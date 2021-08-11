509f4f7a08fb6adb708b729665993481e432edb5
Adroaldo <hygino@msn.com>
2021-08-11 20:41:25 -0300

    Fragment com thymeleaf

---
+++ fragment/src/test/java/com/codegate01/fragment/FragmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegate01.fragment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FragmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
